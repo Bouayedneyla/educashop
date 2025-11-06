54574c50941a028fa87ffecbdbc138771b9e7c42
mariasad227 <mariasaadi227@gmail.com>
2025-11-06 22:15:59 +0100

    Update ShoppingCartPage.java

--- edushop/Cart/ShoppingCartPage.java
+++ edushop/Cart/ShoppingCartPage.java
@@ -1,4 +1,19 @@
 package cart;
 public class ShoppingCartPage{
-    String 
+// This panel will hold all the items in the cart
+private JPanel cartItemsPanel; 
+// You will add/remove items from this panel
+
+// --- Order Summary (Page 9) ---
+private JLabel subtotalLabel;
+private JLabel taxLabel;
+private JLabel totalLabel;
+
+private JTextField promoCodeField;
+private JButton redeemButton;
+private JButton proceedToPaymentButton;
+
+// --- Data ---
+// A list of items in the cart
+private List<OrderItem> items; 
 }
