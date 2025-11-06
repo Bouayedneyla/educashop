eb1f41670e985bf0a26bc193c04ace00420edc53
mariasad227 <mariasaadi227@gmail.com>
2025-11-06 22:27:51 +0100

    Enhance FaqPolicyPage with tabbed interface
    Added JTabbedPane and panels for FAQ and policy sections.

--- edushop/Dashboard/FaqPolicyPage.java
+++ edushop/Dashboard/FaqPolicyPage.java
@@ -1,4 +1,9 @@
 package Dashboard;
 public class FaqPolicyPage{
- String 
+ // This is the perfect place to use a JTabbedPane
+private JTabbedPane tabbedPane;
+
+// --- Panels for each tab ---
+private JPanel faqPanel; // Will contain JLabels for Q&A
+private JPanel policyPanel; // Will contain a JTextArea for Terms of Service 
 }
